9deaa85779ee5aef5b7ab8e01d314995f8dd3b1f
Kyle Fang <zhigang1992@gmail.com>
2019-08-25 20:28:32 +0800

    docs: add react reference doc

--- docs/stories/index.js
+++ docs/stories/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component } from 'react';
 import Animation from 'lottie-react-native';
 
 import { storiesOf } from '@storybook/react';
@@ -7,3 +7,22 @@ import { storiesOf } from '@storybook/react';
 storiesOf('Lottie', module)
   .add('basic', () => <Animation source={require('./assets/gift.json')} autoPlay />)
   .add('loop', () => <Animation source={require('./assets/gift.json')} loop autoPlay />);
+
+
+class ReactReference extends Component {
+  constructor(props) {
+    super(props);
+    this.animationRef = React.createRef();
+  }
+  render() {
+    return <Animation ref={this.animationRef} source={require('./assets/gift.json')} />
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.animationRef.current.play()
+    }, 1000)
+  }
+}
+
+storiesOf('Manual trigger', module)
+  .add('React Ref', () => <ReactReference />);
